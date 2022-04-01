b4f4708f27b1dd35425ff514519a590eccac0d66
WU Chenya <84781745+attendrers@users.noreply.github.com>
2022-04-01 10:58:28 +0200

    Create IR.h

---
+++ compiler/IR.h
@@ -0,0 +1,50 @@
+#ifndef IR_H
+#define IR_H
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <initializer_list>
+
+// Declarations from the parser -- replace with your own
+#include "type.h"
+#include "symbole.h"
+
+class BasicBlock;
+class CFG;
+class DefFonction;
+
+
+//! The class for one 3-address instruction
+class IRInstr {
+ 
+   public:
+	/** The instructions themselves -- feel free to subclass instead */
+	typedef enum {
+		ldconst,
+		copy,
+		add,
+		sub,
+		mul,
+		rmem,
+		wmem,
+		call, 
+		cmp_eq,
+		cmp_lt,
+		cmp_le
+	} Operation;
+
+
+	/**  constructor */
+	IRInstr(BasicBlock* bb_, Operation op, Type t, vector<string> params);
+	
+	/** Actual code generation */
+	void gen_asm(ostream &o); /**< x86 assembly code generation for this IR instruction */
+	
+ private:
+	BasicBlock* bb; /**< The BB this instruction belongs to, which provides a pointer to the CFG this instruction belong to */
+	Operation op;
+	Type t;
+	vector<string> params; /**< For 3-op instrs: d, x, y; for ldconst: d, c;  For call: label, d, params;  for wmem and rmem: choose yourself */
+	// if you subclass IRInstr, each IRInstr subclass has its parameters and the previous (very important) comment becomes useless: it would be a better design. 
+};
